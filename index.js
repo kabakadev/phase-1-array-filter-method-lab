a70c3389f5cab64a35b50125f83e444ee962fcd2
Ian Kabaka <iankabaka1@gmail.com>
2024-09-29 11:08:52 +0300

    filter method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arrayName, stringArg) {
+  return arrayName.filter(
+    (matching) => stringArg.toLowerCase() === matching.toLowerCase()
+  );
+}
+function fuzzyMatch(arrayName, stringArg) {
+  return arrayName.filter((matching) => matching.startsWith(stringArg));
+}
+function matchName(arrayName, stringArg) {
+  return arrayName.filter((matching) => matching.name === stringArg);
+}
